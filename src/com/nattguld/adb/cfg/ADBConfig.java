dcf96662c874e36a29b06395623a0591a847f6ef
Nattguld <6783744+randqm@users.noreply.github.com>
2019-07-04 19:53:17 +0200

    Merged with data

--- src/com/nattguld/adb/cfg/ADBConfig.java
+++ src/com/nattguld/adb/cfg/ADBConfig.java
@@ -1,36 +1,48 @@
 package com.nattguld.adb.cfg;
 
+import java.util.Objects;
+
+import com.nattguld.data.cfg.Config;
+import com.nattguld.data.cfg.ConfigManager;
+import com.nattguld.data.json.JsonReader;
+import com.nattguld.data.json.JsonWriter;
+
 /**
  * 
  * @author randqm
  *
  */
 
-public class ADBConfig {
-	
-	private static ADBConfig singleton;
+public class ADBConfig extends Config {
 	
 	/**
 	 * The SDK tools path.
 	 */
 	private String sdkToolsPath;
 	
+	/**
+	 * The error ip.
+	 */
+	private String errorIP;
+	
 
-/*
+
 	@Override
 	protected void read(JsonReader reader) {
 		this.sdkToolsPath = reader.getAsString("sdk_tools_path", null);
+		this.errorIP = reader.getAsString("error_ip", null);
 	}
 
 	@Override
 	protected void write(JsonWriter writer) {
 		writer.write("sdk_tools_path", sdkToolsPath);
+		writer.write("error_ip", errorIP);
 	}
 	
 	@Override
 	protected String getSaveFileName() {
 		return ".adb_config";
-	}*/
+	}
 	
 	/**
 	 * Modifies the SDK tools path.
@@ -54,15 +66,42 @@ public class ADBConfig {
 	}
 	
 	/**
+	 * Modifies the error IP.
+	 * 
+	 * @param errorIP The new error IP.
+	 * 
+	 * @return The config.
+	 */
+	public ADBConfig setErrorIP(String errorIP) {
+		this.errorIP = errorIP;
+		return this;
+	}
+	
+	/**
+	 * Retrieves the error IP.
+	 * 
+	 * @return The error IP.
+	 */
+	public String getErrorIP() {
+		return errorIP;
+	}
+	
+	/**
+	 * Retrieves whether an error IP is set or not.
+	 * 
+	 * @return The result.
+	 */
+	public boolean hasErrorIP() {
+		return Objects.nonNull(getErrorIP()) && !getErrorIP().isEmpty();
+	}
+	
+	/**
 	 * Retrieves the config.
 	 * 
 	 * @return The config.
 	 */
 	public static ADBConfig getConfig() {
-		if (singleton == null) {
-			singleton = new ADBConfig();
-		}
-		return singleton;
+		return (ADBConfig)ConfigManager.getConfig(new ADBConfig());
 	}
  
 }
